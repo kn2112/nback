85323c73610f5104187cfbfff8b261604eeac947
Kevin <itdept@gardnerlewis.com>
2026-09-18 13:46:18 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // Bump VERSION whenever you change any file in ASSETS. The app shows an
 // "Update available" toast when a new version is waiting.
 
-const VERSION = '1.0.1';
+const VERSION = '1.0.2';
 const CACHE_NAME = `nback-v${VERSION}`;
 
 const ASSETS = [
